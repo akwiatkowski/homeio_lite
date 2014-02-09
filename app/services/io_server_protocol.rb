52734730cf54d584a105df5b3a36e125398e2d93
Aleksander Kwiatkowski <bobikx@poczta.fm>
2014-02-09 12:40:44 +0100

    ioserver protocol/client

---
+++ app/services/io_server_protocol.rb
@@ -0,0 +1,25 @@
+require 'socket'
+
+class IoServerProtocol
+  @@host = IO_SERVER[:host]
+  @@port = IO_SERVER[:port]
+
+  def self.c(command, response_size)
+    s = TCPSocket.open(@@host, @@port)
+
+    # <count of command bytes> <count of response bytes> <command bytes>
+    str = command.size.chr + response_size.to_i.chr + command.to_s
+    s.puts str
+    data = s.gets
+    s.close
+
+    int_data = 0
+    data.each_byte do |b|
+      # puts b
+      int_data *= 256
+      int_data += b
+    end
+
+    return int_data
+  end
+end
---
+++ config/backend/io_server.rb
@@ -0,0 +1,5 @@
+IO_SERVER = {
+  #host: 'localhost',
+  host: '192.168.0.2',
+  port: '2002'
+}
