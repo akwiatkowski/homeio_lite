04197b8cf2f824bbb617aa3f6c2a64c95a492b72
Aleksander Kwiatkowski <bobikx@poczta.fm>
2014-02-21 15:16:02 +0100

    simple overseer

--- app/overseers/simple_overseer.rb
+++ app/overseers/simple_overseer.rb
@@ -1,4 +1,6 @@
 class SimpleOverseer
+  attr_reader :threshold, :type
+
   def initialize(meas_name, action_name, type, threshold, avg_from)
     @meas_cache_storage = MeasCacheStorage[meas_name]
     @action_cache_storage = ActionCacheStorage[action_name]
@@ -6,13 +8,41 @@ class SimpleOverseer
     @typ = type
     @threshold = threshold
     @avg_from = avg_from
+    @type = type
+
+    if type.to_s == "higher"
+      @hit = Proc.new do
+        higher_hit?
+      end
+    elsif type.to_s == "lower"
+      @hit = Proc.new do
+        lower_hit?
+      end
+    else
+      @hit = nil
+    end
+
+
   end
 
   def check
-    logger.debug "#{self.class.to_s} check, value #{value}"
+    logger.debug "#{self.class.to_s} check, value #{value} #{type} #{threshold}, hit #{hit?}"
   end
 
   def value
-    @meas_cache_storage.avg_buffer_value(0, @avg_from)
+    _v = @meas_cache_storage.avg_buffer_value(0, @avg_from)
+    return _v
+  end
+
+  def lower_hit?
+    self.value <= self.threshold
+  end
+
+  def higher_hit?
+    self.value >= self.threshold
+  end
+
+  def hit?
+    @hit.call
   end
 end
