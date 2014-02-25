ea41bca20ff4436ca114fdda4dff58923f4eae7e
Aleksander Kwiatkowski <bobikx@poczta.fm>
2014-02-25 01:06:54 +0100

    temporary custom meas txt string

---
+++ app/controllers/temp_controller.rb
@@ -0,0 +1,22 @@
+HomeioLite::App.controller :measurements do
+  get :show, map: "/meas_type_groups/1/latest.txt" do
+    h = Hash.new
+    m = MeasCacheStorage["batt_u"]
+    d = m.buffer_values_time(0, 0)
+    h[:time] = Time.at(d[0][0])
+    h[:batt_u] = d[0][1]
+
+    m = MeasCacheStorage["i_gen_batt"]
+    d = m.buffer_values_time(0, 0)
+    h[:i_gen_batt] = d[0][1]
+
+    m = MeasCacheStorage["coil_1_u"]
+    d = m.buffer_values_time(0, 0)
+    h[:coil_1_u] = d[0][1]
+
+
+    string = "#{"%.1f" % h[:batt_u]} V | #{"%.1f" % h[:i_gen_batt]} A | #{"%.1f" % h[:coil_1_u]} V | #{h[:time].strftime("%H:%M:%S")}"
+    string
+  end
+end
+
