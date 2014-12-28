522b104be679546d4067d3e58965eedc18d6f876
Aleksander Kwiatkowski <bobikx@poczta.fm>
2014-12-28 18:31:29 +0100

    fix

--- backend/fake_server/s_backend_server.rb
+++ backend/fake_server/s_backend_server.rb
@@ -25,6 +25,8 @@ loop do
     from = $2.to_i
     to = $3.to_i
 
+    puts "from #{from} to #{to}"
+
     if meas_types[name].nil?
       meas_types[name] = Array.new
       meas_types[name] << rand(256)
@@ -36,12 +38,28 @@ loop do
     while enough == false
       value = meas_types[name].last
 
+      if value < 50
+        value += 2
+      elsif value < 90
+        value += 1
+      elsif value > (512 - 90)
+        value -= 1
+      elsif value > (512 - 50)
+        value += 2
+      end
+
       rest = rand(100) % 6
       if rest == 2 or rest == 3
         # nothing
       else
         value += 6 - rest
       end
+
+      # some more funny stuff
+      value += (3.0 * Math.sin(meas_types[name].size.to_f / 10.0)).to_i
+
+      value = 0 if value < 0
+      value = 512 if value > 512
 
       meas_types[name] << value
 
@@ -50,8 +68,10 @@ loop do
 
     last_times[name] = Time.now.to_i
 
-    fixed_from = meas_types[name].size - from - 1
-    fixed_to = meas_types[name].size - to - 1
+    fixed_to = meas_types[name].size - from - 1
+    fixed_from = meas_types[name].size - to - 1
+
+    puts "fixed_from #{fixed_from} fixed_to #{fixed_to}"
 
     response = {
       status: 0,
@@ -60,7 +80,7 @@ loop do
       first_time: first_times[name],
       interval: interval,
       count: meas_types[name].size,
-      data: meas_types[name][fixed_from..fixed_to]
+      data: meas_types[name][fixed_from..fixed_to].reverse
     }.to_json
 
   end
